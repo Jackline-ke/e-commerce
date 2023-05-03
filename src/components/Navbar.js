f22bafdac1e0ed52c87312c57205bb902c52a7b1
JAFFAR NGAIRA <garsolinajaffar@gmail.com>
2023-05-03 12:53:19 +0300

    completed assignment

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import { ShoppingCart } from 'phosphor-react';
 
 function Navbar() {
     return (
-        <div>
+        <div className='navbar'>
             <Link to="/">Home</Link>
             <Link to="/Cart">
                 <ShoppingCart />
